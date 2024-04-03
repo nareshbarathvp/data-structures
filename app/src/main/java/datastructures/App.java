63b5cb54eb62cecca883551e54818730069fa074
nareshbarath <nareshbarath2714@gmail.cocm>
2024-04-03 21:53:54 +0530

    initial commit

---
+++ app/src/main/java/datastructures/App.java
@@ -0,0 +1,11 @@
+package datastructures;
+
+public class App {
+    public String getGreeting() {
+        return "Data structures";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
